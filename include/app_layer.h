e21809ddcdd72b5a21ebf66cbbf4162afb2da98d
griffinross2 <griffinross2@gmail.com>
2025-10-15 18:52:44 -0400

    Update app_layer.h

--- include/app_layer.h
+++ include/app_layer.h
@@ -1,12 +1,14 @@
 #pragma once
 
 class AppLayer {
-	
 public:
-	AppLayer() = default;
-	~AppLayer() = default;
-	
-	virtual void Update() {}
-    virtual void Render() {}
+    AppLayer() = default;
+    ~AppLayer() = default;
 
+    virtual void Update() {}
+    virtual void Render() {}
+    void SetVisible(bool visible) { m_visible = visible; }
+
+protected:
+    bool m_visible = true;
 };
